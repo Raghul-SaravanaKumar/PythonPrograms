5ee92e2af7d8f5db11cca7296908d89353a9795a
Raghul Saravanakumar <rexizzz1815@gmail.com>
2026-06-19 21:44:47 +0530

    Fix vowel counting output message

---
+++ Count Vowels.py
@@ -0,0 +1,10 @@
+Name = str(input("Enter a string: "))
+count = 0
+for i in Name:   
+    if i in 'aeiouAEIOU':
+       count +=1 
+print("Vowels present in " + Name + " is:",count)
+
+    
+
+       
